64499d60082356371403831b824af5e5965b5c2c
Nemanja Blagojević <Nemanja1906>
2025-07-08 15:06:18 +0200

    Adding Dark and Light Theme

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/providers/theme_provider.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    return Scaffold(
+      body:Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SwitchListTile(
+            title:  Text(themeProvider.getIsDarkTheme ? "Dark Mode" : "Light Mode"),
+            value: themeProvider.getIsDarkTheme, 
+            onChanged: (value) {
+
+              themeProvider.setDarkTheme(themevalue: value);
+            }, 
+          )
+        ],
+      )
+
+    );
+  }
+}
